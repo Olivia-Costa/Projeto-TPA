19dd23d6e62ed214b9a0a5f55fee86fc62e0885b
Olivia-Costa <costaolivia54@gmail.com>
2026-04-24 11:41:21 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/firebase_options.dart';
+import 'package:flutter_application_1/src/app.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  //chama a tela inicial do APP
+  runApp(FilmesApp());
+}
